acfc5f78c14b21c1123500d0e2015f988f22c801
João G .Albuquerque <jg5900212@gmail.com>
2024-10-24 01:54:38 -0300

    enviar.php

--- enviar.php
+++ enviar.php
@@ -1,21 +1,26 @@
 <?php
+// Obtém os dados do formulário usando o método POST e escapa caracteres especiais para evitar problemas de segurança
+$nome = addslashes($_POST['nome']);
+$email = addslashes($_POST['email']);
+$filme = addslashes($_POST['Filme']);
+$formadepagamento = addslashes($_POST['Forma de pagamento']);
 
-$nome = addcslashes($_POST['nome']);
-$email = addcslashes($_POST['email']);
-$celular = addcslashes($_POST['filme']);
-$mensagem = addcslashes($_POST['formadepagamento']);
+// Define o endereço de e-mail para onde a mensagem será enviada
+$para = "suportplayfilmes@gmail.com";
 
-$para = "suporteascentgroup@gmail.com";
+// Define o assunto do e-mail
 $assunto = "Contato do site";
 
-$corpo = "Nome: " . $nome . "\n". "E-mail: " .$email."\n" ."Filme: " .$filme."\n". "Forma de pagamento: " .$formadepagamento;
+// Monta o corpo do e-mail com as informações recebidas do formulário
+$corpo = "Nome: " . $nome . "\n" . "E-mail: " . $email . "\n" . "Filme: " . $filme . "\n" . "Forma de pagamento: " . $formadepagamento;
 
-$importante = "From: jg5900212@gmail.com"."\n"."Reply-to: ".$email."\n"."X=Malier:PHP/".phpversion()
+// Define os cabeçalhos do e-mail, incluindo o endereço do remetente e a versão do PHP utilizada
+$headers = "From: jg5900212@gmail.com" . "\n" . "Reply-to: " . $email . "\n" . "X-Mailer: PHP/" . phpversion();
 
-if(mail($para, $assunto, $corpo, $importante)){
+// Envia o e-mail usando a função mail(). Se o envio for bem-sucedido, exibe uma mensagem de sucesso. Caso contrário, exibe uma mensagem de erro.
+if (mail($para, $assunto, $corpo, $headers)) {
     echo("Email enviado com sucesso!");
-}else{
+} else {
     echo("Falha ao enviar o email");
 }
-
 ?>
